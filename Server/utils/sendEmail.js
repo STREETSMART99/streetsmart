171a65a3d880c7a4df3058502ac84f57d3f7e2c4
STREETSMART99 <nightshifters99@gmail.com>
2025-09-02 21:47:34 +0800

    Update sendEmail.js

--- Server/utils/sendEmail.js
+++ Server/utils/sendEmail.js
@@ -27,11 +27,6 @@ const sendEmail = async (to, subject, htmlContent) => {
 
 };
 
-const clientUrl = process.env.CLIENT_URL;
-const verificationLink = `${clientUrl}/verify-email/${token}`;
-const htmlContent = `<a href="${verificationLink}">Verify your email</a>`;
-sendEmail(user.email, "Verify your email", htmlContent);
-
 module.exports = sendEmail;
 
 
