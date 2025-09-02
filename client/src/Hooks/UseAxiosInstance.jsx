39e3818042546c73b7d5552ffcd1651da8477f75
STREETSMART99 <nightshifters99@gmail.com>
2025-09-02 20:43:58 +0800

    Update UseAxiosInstance.jsx

--- client/src/Hooks/UseAxiosInstance.jsx
+++ client/src/Hooks/UseAxiosInstance.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api/auth', // backend url
+    // baseURL: 'http://localhost:5000/api/auth', // backend url
+    baseURL: 'https://streetsmart-server.onrender.com/api/auth',
     withCredentials: true,
     headers: {
         'Content-type': 'application/json'
